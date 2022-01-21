942577b346b01799f540b82d1d7ac48859a31eb1
ayunovita <christinaoctavia62@gmail.com>
2022-01-21 10:29:13 +0700

    new commit

--- app/src/main/java/com/example/mymemo/MainActivity.java
+++ app/src/main/java/com/example/mymemo/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.mymemo;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Calendar myCalendar;
+    DatePickerDialog.OnDateSetListener date;
+    EditText txt_tgl, txt_jam, txt_personal;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_tgl = (EditText) findViewById(R.id.txt_tgl);
+        txt_jam = (EditText) findViewById(R.id.txt_jam);
+        txt_personal = (EditText) findViewById(R.id.txt_personal);
+
+        myCalendar = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        txt_tgl.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        txt_personal.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_YEAR)).show();
+            }
+        });
+
+        txt_jam.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        String jam = "" + selectedHour;
+                        String menit = "" + selectedMinute;
+
+                        txt_jam.setText( "     " + selectedHour + " : " + selectedMinute);
+
+
+
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle(    "Select Time");
+                mTimePicker.show();
+
+            }
+        });
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void updateLabel() {
+        String myFormat = "     yyyy-MM-dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        txt_tgl.setText(sdf.format(myCalendar.getTime()));
+        String Format = "yyyy-MM-dd";
+        txt_personal.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
